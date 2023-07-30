1aafe7c6dab376b75c88f4448fdbf2329944dc10
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-30 16:37:21 +0200

    2 files modified, 1 added

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ShapesService } from '../shapes.service';
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.scss'],
 })
 export class SecondComponent implements OnInit {
-  constructor() {}
+  shape = '';
+  color = '';
+  shapeClass = '';
 
-  ngOnInit() {}
+  constructor(private shapesService: ShapesService) {}
+
+  ngOnInit(): void {}
+
+  drawShape() {
+    this.shapeClass = this.shapesService.getClassName(this.shape, this.color);
+  }
 }
